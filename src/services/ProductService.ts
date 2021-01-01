22a93fac991fdd16f30c7d72ffc87af589af04f8
Marcio Alves <malvesb@thoughtworks.com>
2021-01-01 20:03:28 -0300

    create product service unit tests

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,5 +1,4 @@
-import Logger from '../commons/Logger';
-import Product, { IProduct } from '../models/Product';
+import { IProduct } from '../models/Product';
 
 export const search = (filter: string) : Promise<IProduct[]> => {
   return Promise.reject()
