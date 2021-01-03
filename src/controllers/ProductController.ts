458c5461610eee5608459ce4e9f2b0e5980a70de
Marcio Alves <malvesb@thoughtworks.com>
2021-01-03 05:15:56 -0300

    create paging logic

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -6,8 +6,8 @@ import Logger from '../commons/Logger'
 export const index : RequestHandler = async (req: Request, res: Response) => {
   try {
     const filter: string = req.query.filter as string
-    const page = req.query.page as any | 1
-    const products = await search(filter, page)
+    const page = req.query.page as string
+    const products = await search(filter, Number.parseInt(page))
     return res.json(products)
   } catch (error) {
     Logger.error(error)
